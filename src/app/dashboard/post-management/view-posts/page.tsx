17d87e7eaffe32fc9b53a1c8de9a7ea9b4944d19
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-10-20 22:05:12 +0800

    feat: Initialize view posts page

--- src/app/dashboard/post-management/view-posts/page.tsx
+++ src/app/dashboard/post-management/view-posts/page.tsx
@@ -1,9 +1,9 @@
-import EditorComponent from "@/components/Forms/PostManagementForms/EditorComponent";
+import PostListComponent from "@/components/Forms/PostManagementForms/PostListComponent";
 export default function Page() {
     return (
         <div>
             <div className="flex flex-col flex-1 p-4 pt-0">
-                <EditorComponent />
+                <PostListComponent />
             </div>
         </div>
     )
