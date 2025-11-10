1ba03712cee6ad609e29b544fc8d14cff3b3bcd1
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-11-11 03:49:18 +0800

    chore: Remove stations list feature from construction

--- src/app/dashboard/station-management/page.tsx
+++ src/app/dashboard/station-management/page.tsx
@@ -1,10 +1,7 @@
 import StationManagementList from "@/components/Forms/StationManagementForms/StationManagementList";
-import UnderConstruction from "@/components/UnderConstruction";
 
 export default function Page() {
     return (
-        <UnderConstruction>
-            <StationManagementList />
-        </UnderConstruction>
+        <StationManagementList />
     )
 }
