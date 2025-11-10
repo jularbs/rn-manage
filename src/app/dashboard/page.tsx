bb77a87a25a8ac44eb92ed4fb87233097b5848fc
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-11-11 03:31:11 +0800

    feat: show landing message

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import UnderConstruction from "@/components/UnderConstruction";
 
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <UnderConstruction>
-        <h1>Hello World</h1>
-      </UnderConstruction>
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <p>Welcome to Radyo Natin Content Management System!</p>
     </div>
   )
 }
