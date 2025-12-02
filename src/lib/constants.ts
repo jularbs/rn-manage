94510f89ff06418ad397dc4b6eb7216bea7a38d4
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-12-02 15:58:51 +0800

    #RNW-8: Add page slug constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,13 @@ export const ACCEPTED_IMAGE_TYPES = [
   "image/webp",
 ];
 
+// Static page slugs
+export const ABOUT_US_SLUG = "about-us";
+export const CONTACT_US_SLUG = "contact-us";
+export const TERMS_OF_USE_SLUG = "terms-of-use";
+export const PRIVACY_POLICY_SLUG = "privacy-policy";
+export const ADVERTISE_WITH_US_SLUG = "advertise-with-us";
+
 // User Designations
 export const ADMIN_ROLE = "admin";
 export const MANAGER_ROLE = "manager";
