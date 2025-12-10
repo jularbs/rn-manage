5967c1df1ac635bda0bf50309f542167adb0c0ef
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-12-10 18:58:24 +0800

    fix: upgrade nextjs for RCE vulnerability and fix breaking changes

--- src/app/dashboard/page-management/[slug]/page.tsx
+++ src/app/dashboard/page-management/[slug]/page.tsx
@@ -1,9 +1,10 @@
 import PageEditorComponent from "@/components/Forms/PageManagementForms/PageEditorComponent";
-export default function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params;
     return (
         <div>
             <div className="flex flex-col flex-1 p-4 pt-0">
-                <PageEditorComponent slug={params.slug} />
+                <PageEditorComponent slug={slug} />
             </div>
         </div>
     )
